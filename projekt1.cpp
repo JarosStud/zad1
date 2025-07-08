26e0f127a158a923861795d11f77a05983f8bea0
JarosStud <jaros@mat.umk.pl>
2025-07-08 19:37:57 +0200

    dodanie funkcji silni

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,6 +2,14 @@
 
 using namespace std;
 
+int silnia(int a){
+	buf = 1;
+	for(int i=1; i<a;i++){
+		buf=buf*i;
+	};
+	return buf;
+};
+
 int main() {
     int a, b;
     cout << "Podaj liczbe calkowita ";
